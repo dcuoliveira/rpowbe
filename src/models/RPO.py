1034a3e98df4a382082b7c9eb5d452de8901375d
grover002 <grover.org.rf@gmail.com>
2023-08-08 22:04:36 +0200

    First implementation of robust optimization method when the uncertainty set is quadratic, without estimators for the uncertainty set.

---
+++ src/models/RPO.py
@@ -0,0 +1,97 @@
+import torch
+import numpy as np
+# uses optimization methods from CVXOPT
+import picos as pc
+import scipy.optimize as opt
+
+from estimators.Estimators import Estimators
+
+class RPO(Estimators):
+    def __init__(self,
+                 risk_aversion: float=1.0,
+                 uncertainty: float=1.0,
+                 omega_estimator: str="mle",
+                 mean_estimator: str="mle",
+                 covariance_estimator: str="mle") -> None:
+        """"
+        This function impements the Robust Portofolio Optimization with quadratic uncertainty set.
+
+        Args:
+            risk_aversion (float): risk aversion parameter. Defaults to 0.5. 
+                                   The risk aversion parameter is a scalar that controls the trade-off between risk and return.
+                                   According to Ang (2014), the risk aversion parameter of a risk neutral individual ranges from 1 and 10.
+            omega_estimator: Uncertainty set estimator to be used (always quadratic). Defaults to "mle", which defines the maximum likelihood estimator.
+            uncertainty (float): uncertainty parameter. Defaults to 1.
+                                 This parameter controls the size of the elipsoidal uncertainty set.
+            mean_estimator (str): mean estimator to be used. Defaults to "mle", which defines the maximum likelihood estimator.
+            covariance_estimator (str): covariance estimator to be used. Defaults to "mle", which defines the maximum likelihood estimator.
+
+        References:
+        Markowitz, H. (1952) Portfolio Selection. The Journal of Finance.
+        Ang, Andrew, (2014). Asset Management: A Systematic Approach to Factor Investing. Oxford University Press. 
+        """
+        super().__init__()
+        
+        self.risk_aversion = risk_aversion
+        self.uncertainty = uncertainty
+        self.omega_estimator = omega_estimator
+        self.mean_estimator = mean_estimator
+        self.covariance_estimator = covariance_estimator
+
+    def forward(self,
+                returns: torch.Tensor,
+                num_timesteps_out: int,
+                long_only: bool=True,
+                verbose: bool=True) -> torch.Tensor:
+
+        N = returns.shape[1]
+
+        # mean estimator
+        if self.mean_estimator == "mle":
+            mean_t = self.MLEMean(returns)
+        else:
+            raise NotImplementedError
+
+        # covariance estimator
+        if self.covariance_estimator == "mle":
+            cov_t = self.MLECovariance(returns)
+        else:
+            raise NotImplementedError
+        
+        # uncertainty set estimator
+        if self.omega_estimator == "mle":
+            omega_t = torch.eye(N)
+        else:
+            raise NotImplementedError
+        # max w^{\top}\bar{u} - (\kappa)*\sqrt(w^{\top} \Omega w) - \frac{\lambda}{2}w^{\top} \Sigma w
+        #print(pc.available_solvers())
+        # Problem
+        def objective(weights):
+            return np.dot(mean_t, weights) - self.uncertainty*np.sqrt(np.dot(weights,np.dot(omega_t,weights))) - (self.risk_aversion/2)*np.dot(weights,np.dot(cov_t,weights))
+
+        if long_only:
+            constraints = [
+                {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}  # The weights sum to one
+            ]
+            bounds = [(0, None) for _ in range(N)]
+        else:
+            constraints = [
+                {'type': 'eq', 'fun': lambda x: np.sum(x) - 0},  # the weights sum to zero
+                {'type': 'eq', 'fun': lambda x: np.sum(np.abs(x)) - 1},  # the sum of absolute weights is one
+                {'type': 'eq', 'fun': lambda x: np.linalg.norm(x) - 1}  # the norm L2 is one
+            ]
+            bounds = None
+
+        # initial guess for the weights
+        x0 = np.ones(N) / N
+
+        # Perform the optimization
+        opt_output = opt.minimize(objective, x0, constraints=constraints, bounds=bounds, method='SLSQP')
+        wt = torch.tensor(np.array(opt_output.x)).T.repeat(num_timesteps_out, 1)
+
+        return wt
+    
+    def forward_analytic(self,
+                         returns: torch.Tensor,
+                         num_timesteps_out: int) -> torch.Tensor:
+        pass
