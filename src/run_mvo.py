ff3028815158532451dc1d5e388e62a67f78c5bb
dcuoliveira <dan.cunhaa@gmail.com>
2023-09-17 18:30:17 -0300

    add small data

--- src/run_mvo.py
+++ src/run_mvo.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    print("Running script with the following parameters: model_name: {}, use_sample_data: {}, all_years: {}, long_only: {}, mean_estimator: {}, covariance_estimator: {}".format(args.model_name, args.use_sample_data, args.all_years, args.long_only, args.mean_estimator, args.covariance_estimator))
+    print("Running script with the following parameters: model_name: {}, use_small_data {}, use_sample_data: {}, all_years: {}, long_only: {}, mean_estimator: {}, covariance_estimator: {}".format(args.model_name, args.use_small_data, args.use_sample_data, args.all_years, args.long_only, args.mean_estimator, args.covariance_estimator))
 
     model_name = args.model_name
     train_ratio = 0.6
