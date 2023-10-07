a95504cd30c79387aa09be82399ad7454fe65375
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-07 17:35:41 -0300

    add rbmvo without gradient

--- src/models/RBMVO.py
+++ src/models/RBMVO.py
@@ -5,15 +5,19 @@ import scipy.optimize as opt
 
 from estimators.Estimators import Estimators
 
-# Robust bootstrap MVO
 class RBMVO(Estimators):
     def __init__(self,
                  risk_aversion: float=1,
-                 mean_estimator: str="mle",
-                 covariance_estimator: str="mle",
-                 num_boot: int = 200) -> None:
+                 mean_cov_estimator: str="mle",
+                 num_boot: int = 200,
+                 alpha: float = 0.95) -> None:
         """"
-        This function impements the mean-variance optimization (MVO) method proposed by Markowitz (1952).
+        This function impements the robust version of the mean-variance optimization (MVO) method proposed by Markowitz (1952).
+        It intends to be robust in two senses:
+            1. It uses a bootstrap procedure to estimate confidence intervals of the mean and covariance of returns.
+            2. It uses the alpha-percentile mean and covariance as a plug-in estimator for the mean and covariance of returns.
+
+        Optimization can be done using: (a) gradient descent or (b) using the scipy.optimize.minimize function.
 
         Args:
             risk_aversion (float): risk aversion parameter. Defaults to 0.5. 
@@ -29,9 +33,10 @@ class RBMVO(Estimators):
         super().__init__()
         
         self.risk_aversion = risk_aversion
-        self.mean_estimator = mean_estimator 
-        self.covariance_estimator = covariance_estimator
+        self.mean_cov_estimator = mean_cov_estimator 
+        self.covariance_estimator = mean_cov_estimator
         self.num_boot = num_boot
+        self.alpha = alpha
         self.list_mean_covs = list()
 
     def objective(self,
@@ -45,24 +50,76 @@ class RBMVO(Estimators):
     def forward(self,
                 returns: torch.Tensor,
                 num_timesteps_out: int,
-                long_only: bool=True) -> torch.Tensor:
-        
+                long_only: bool=True):
+             
         K = returns.shape[1]
 
-        # mean estimator
-        if self.mean_estimator == "mle":
+        # mean and cov estimates
+        if self.mean_cov_estimator == "mle":
             self.list_mean_covs = [self.MLEMean(returns)]
         else:
             self.list_mean_covs = self.DependentBootstrapMean_Covariance(returns=returns,
-                                                        boot_method=self.mean_estimator,
-                                                        Bsize=50,
-                                                        rep=self.num_boot)
+                                                                         boot_method=self.mean_cov_estimator,
+                                                                         Bsize=50,
+                                                                         rep=self.num_boot)
+        
+        wt = torch.Tensor(np.random.uniform(-1, 1, size = K))
+        # compute utilities for all bootstraps
+        bootstrap_utilities = list()
+        for idx in range(len(self.list_mean_covs)):
+            mean_i, cov_i = self.list_mean_covs[idx]
+            utility = torch.matmul(wt, mean_i) - self.risk_aversion*torch.matmul(wt, torch.matmul(cov_i, wt))
+            bootstrap_utilities.append(utility.item())
+            
+        # sort the utilities  with index
+        idxs_bootstrap_utilities_sorted = sorted(range(len(bootstrap_utilities)), key=lambda k: bootstrap_utilities[k])
+        idx_IC = idxs_bootstrap_utilities_sorted[int(self.alpha*self.num_boot)]
+
+        # compute the derivative
+        self.mean_t, self.cov_t = self.list_mean_covs[idx_IC] 
+
+        if long_only:
+            constraints = [
+                {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}  # the weights sum to one
+            ]
+            bounds = [(0, None) for _ in range(K)]
+
+            w0 = np.random.uniform(0, 1, size=K)
+        else:
+            constraints = [
+                {'type': 'eq', 'fun': lambda x: np.sum(x) - 0},  # the weights sum to zero
+                {'type': 'eq', 'fun': lambda x: np.sum(np.abs(x)) - 1},  # the weights sum to zero
+            ]
+            bounds = [(-1, 1) for _ in range(K)]
+
+            w0 = np.random.uniform(-1, 1, size=K)
+
+        # perform the optimization
+        opt_output = opt.minimize(self.objective, w0, constraints=constraints, bounds=bounds, method='SLSQP')
+        wt = torch.tensor(np.array(opt_output.x)).T.repeat(num_timesteps_out, 1)
+
+        return wt
+
+    def forward_gradient(self,
+                         returns: torch.Tensor,
+                         num_timesteps_out: int,
+                         long_only: bool=True) -> torch.Tensor:
+        
+        K = returns.shape[1]
+
+        # mean and cov estimates
+        if self.mean_cov_estimator == "mle":
+            self.list_mean_covs = [self.MLEMean(returns)]
+        else:
+            self.list_mean_covs = self.DependentBootstrapMean_Covariance(returns=returns,
+                                                                         boot_method=self.mean_cov_estimator,
+                                                                         Bsize=50,
+                                                                         rep=self.num_boot)
            
 
-        theta = torch.Tensor(np.random.uniform(-1, 1, size = K))
+        wt = torch.Tensor(np.random.uniform(-1, 1, size = K))
         # Nick's optimization proposal
         step = 0.01 # -> for gradient descent
-        alpha = 0.95
         eps = 1e-6
         num_iter = 100
         while num_iter != 0:
@@ -70,27 +127,24 @@ class RBMVO(Estimators):
             # compute utilities for all bootstraps
             bootstrap_utilities = list()
             for idx in range(len(self.list_mean_covs)):
-                mean_i,cov_i = self.list_mean_covs[idx]
-                utility = torch.matmul(theta,mean_i) - self.risk_aversion*torch.matmul(theta,torch.matmul(cov_i,theta))
+                mean_i, cov_i = self.list_mean_covs[idx]
+                utility = torch.matmul(wt, mean_i) - self.risk_aversion*torch.matmul(wt, torch.matmul(cov_i, wt))
                 bootstrap_utilities.append(utility.item())
 
             # sort the utilities  with index
             idxs_bootstrap_utilities_sorted = sorted(range(len(bootstrap_utilities)), key=lambda k: bootstrap_utilities[k])
-            idx_IC = idxs_bootstrap_utilities_sorted[int(alpha*self.num_boot)]
+            idx_IC = idxs_bootstrap_utilities_sorted[int(self.alpha*self.num_boot)]
 
             # compute the derivative
             mean_IC, cov_IC = self.list_mean_covs[idx_IC] 
-            d_utility_theta = mean_IC - 2*self.risk_aversion*torch.matmul(cov_IC,theta)
-            new_theta = theta + step*d_utility_theta
-            if torch.sum(torch.abs(theta - new_theta)) < eps:
+            d_utility_theta = mean_IC - 2*self.risk_aversion*torch.matmul(cov_IC, wt)
+            new_wt = wt + step*d_utility_theta
+            if torch.sum(torch.abs(wt - new_wt)) < eps:
                 print("convergence attained")
                 break
-            theta = new_theta
+            wt = new_wt
             num_iter = num_iter - 1
 
-        return theta
-    
-    def forward_analytic(self,
-                         returns: torch.Tensor,
-                         num_timesteps_out: int) -> torch.Tensor:
-        pass
+        wt = wt.repeat(num_timesteps_out, 1)
+
+        return wt
