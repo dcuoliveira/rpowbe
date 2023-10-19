b24c98c6fd8fb313fc96de3b41a5cb1a7fdca4da
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-19 17:51:23 -0300

    delete unused param

--- src/run_ew.py
+++ src/run_ew.py
@@ -13,7 +13,6 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--model_name', type=str, help='model name to be used for saving the model', default="ew")
 parser.add_argument('--use_small_data', type=str, help='use sample stocks data', default="False")
-parser.add_argument('--use_sample_data', type=str, help='use sample stocks data', default="False")
 parser.add_argument('--all_years', type=str, help='use all years to build dataset', default="True")
 
 if __name__ == "__main__":
@@ -22,14 +21,12 @@ if __name__ == "__main__":
 
     model_name = args.model_name
     use_small_data = check_bool(args.use_small_data)
-    use_sample_data = check_bool(args.use_sample_data)
     all_years = check_bool(args.all_years)
 
-    print("Running script with the following parameters: model_name: {}, use_small_data {}, use_sample_data: {}, all_years: {}".format(model_name, use_small_data, use_sample_data, all_years))
+    print("Running script with the following parameters: model_name: {}, use_small_data {}, all_years: {}".format(model_name, use_small_data, all_years))
 
     model_name = "{}_lo".format(model_name)
     model_name = "{}_small".format(model_name) if use_small_data else model_name
-    model_name = "{}_sample".format(model_name) if use_sample_data else model_name
     args.model_name = model_name
 
     # relevant paths
@@ -37,7 +34,7 @@ if __name__ == "__main__":
     inputs_path = os.path.join(source_path, "data", "inputs")
 
     # prepare dataset
-    loader = CRSPSimple(use_small_data=use_small_data, use_sample_data=use_sample_data, all_years=all_years)
+    loader = CRSPSimple(use_small_data=use_small_data, all_years=all_years)
     returns = loader.returns.T
     features = loader.features
     features = features.reshape(features.shape[0], features.shape[1] * features.shape[2]).T    
