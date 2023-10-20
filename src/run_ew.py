130f6904f6fba538b5c23d80f34eefac1b6a52c4
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-20 14:47:27 -0300

    ew new version

--- src/run_ew.py
+++ src/run_ew.py
@@ -1,32 +1,24 @@
 import os
 import pandas as pd
-import torch
 import argparse
-import json
 
 from models.EW import EW
-from data.CRSPSimple import CRSPSimple
+from data.ETFsLoader import ETFsLoader
 from utils.conn_data import save_result_in_blocks
-from utils.dataset_utils import check_bool
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--model_name', type=str, help='model name to be used for saving the model', default="ew")
-parser.add_argument('--use_small_data', type=str, help='use sample stocks data', default="False")
-parser.add_argument('--all_years', type=str, help='use all years to build dataset', default="True")
 
 if __name__ == "__main__":
 
     args = parser.parse_args()
 
     model_name = args.model_name
-    use_small_data = check_bool(args.use_small_data)
-    all_years = check_bool(args.all_years)
 
-    print("Running script with the following parameters: model_name: {}, use_small_data {}, all_years: {}".format(model_name, use_small_data, all_years))
+    print("Running script with the following parameters: model_name: {}".format(model_name))
 
     model_name = "{}_lo".format(model_name)
-    model_name = "{}_small".format(model_name) if use_small_data else model_name
     args.model_name = model_name
 
     # relevant paths
@@ -34,7 +26,7 @@ if __name__ == "__main__":
     inputs_path = os.path.join(source_path, "data", "inputs")
 
     # prepare dataset
-    loader = CRSPSimple(use_small_data=use_small_data, all_years=all_years)
+    loader = ETFsLoader()
     returns = loader.returns.T
     features = loader.features
     features = features.reshape(features.shape[0], features.shape[1] * features.shape[2]).T    
