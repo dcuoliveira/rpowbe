34d1eb1a87b4e5763a7951de10bc5cb47a05272e
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-20 14:37:57 -0300

    add etfs loader

---
+++ src/data/ETFsLoader.py
@@ -0,0 +1,71 @@
+import os
+import torch
+import pandas as pd
+import glob
+from tqdm import tqdm
+import argparse
+
+class ETFs(object):
+    """
+
+    
+    """
+    
+    def __init__(self):
+        super().__init__()
+
+        self.inputs_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "inputs")
+        self._read_data()
+
+    def _read_data(self):
+        
+        etfs_df = pd.read_csv(os.path.join(self.inputs_path, "etfs.csv"))
+        etfs_df["date"] = pd.to_datetime(etfs_df["date"])
+        etfs_df.set_index("date", inplace=True)
+
+        # dataset processing 1
+        ## sort index
+        etfs_df = etfs_df.sort_index()
+
+        # dataset processing 2
+        ## compute returns and subset data
+        returns_df = etfs_df.dropna().copy()
+
+        # save indexes
+        self.index = list(returns_df.index)
+        self.columns = list(returns_df.columns)
+
+        # create tensor with (num_nodes, num_features_per_node, num_timesteps)
+        num_nodes = returns_df.shape[1]
+        num_features_per_node = 1
+        num_timesteps = returns_df.shape[0]
+
+        features = torch.zeros(num_nodes, num_features_per_node, num_timesteps)
+        returns = torch.zeros(num_nodes, num_timesteps)
+        for i in range(num_nodes):
+            # features
+            features[i, :, :] = torch.from_numpy(returns_df.loc[:, returns_df.columns[i]].values)
+
+            # returns
+            returns[i, :] = torch.from_numpy(returns_df.loc[:, returns_df.columns[i]].values)
+        
+        # create fully connected adjaneccny matrix
+        A = torch.ones(num_nodes, num_nodes)
+
+        self.A = A
+        self.features = features
+        self.returns = returns
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--debug', type=str, help='To run in debug mode.', default="False")
+
+args = parser.parse_args()
+
+if args.debug == "False":
+    DEBUG = False
+else:
+    DEBUG = True
+
+if __name__ == "__main__":
+    if DEBUG:
+        loader = ETFs()
