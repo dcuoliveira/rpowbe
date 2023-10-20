41de31e44973b3f2ff41674866976356b802f873
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-20 15:22:26 -0300

    update runners to etf

--- src/run_rbmvog.py
+++ src/run_rbmvog.py
@@ -6,7 +6,7 @@ import json
 from tqdm import tqdm
 
 from models.RBMVO import RBMVO
-from data.CRSPSimple import CRSPSimple
+from data.ETFsLoader import ETFsLoader
 from utils.dataset_utils import create_rolling_window_ts, check_bool
 from loss_functions.SharpeLoss import SharpeLoss
 from utils.conn_data import save_result_in_blocks
@@ -16,8 +16,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-mn', '--model_name', type=str, help='model name to be used for saving the model', default="rbmvog")
 parser.add_argument('-nti', '--num_timesteps_in', type=int, help='size of the lookback window for the time series data', default=252 * 3)
 parser.add_argument('-nto', '--num_timesteps_out', type=int, help='size of the lookforward window to be predicted', default=1)
-parser.add_argument('--use_small_data', type=str, help='use small sample stocks data', default="False")
-parser.add_argument('-ay', '--all_years', type=str, help='use all years to build dataset', default="False")
 parser.add_argument('-lo', '--long_only', type=str, help='consider long only constraint on the optimization', default="False")
 parser.add_argument('-meancove', '--mean_cov_estimator', type=str, help='name of the estimator to be used for the expected returns', default="cbb")
 
@@ -31,19 +29,15 @@ if __name__ == "__main__":
     num_timesteps_out = args.num_timesteps_out
     fix_start = False
     drop_last = True
-    use_small_data = check_bool(args.use_small_data)
-    all_years = check_bool(args.all_years)
     long_only = check_bool(args.long_only)
     mean_cov_estimator = args.mean_cov_estimator
     alpha = 0.95
     functional = "utility"
 
-    print("Running script with the following parameters: model_name: {}, use_small_data {}, all_years: {}, long_only: {}, mean_cov_estimator: {}".format(model_name, use_small_data, all_years, long_only, mean_cov_estimator))
+    print("Running script with the following parameters: model_name: {}, long_only: {}, mean_cov_estimator: {}".format(model_name, long_only, mean_cov_estimator))
 
     # add tag for long only or long-short portfolios
     model_name = "{model_name}_lo".format(model_name=model_name) if long_only else "{model_name}_ls".format(model_name=model_name)
-
-    model_name = "{}_small".format(model_name) if use_small_data else model_name
 
     # add mean estimator tag to name
     model_name = "{model_name}_{mean_cov_estimator}".format(model_name=model_name, mean_cov_estimator=mean_cov_estimator)
@@ -55,7 +49,7 @@ if __name__ == "__main__":
     inputs_path = os.path.join(source_path, "data", "inputs")
 
     # prepare dataset
-    loader = CRSPSimple(use_small_data=use_small_data, all_years=all_years)
+    loader = ETFsLoader()
     returns = loader.returns.T
     features = loader.features
     features = features.reshape(features.shape[0], features.shape[1] * features.shape[2]).T  
