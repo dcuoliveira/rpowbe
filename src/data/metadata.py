8e3018151a15e31d9991ad5594c36711374d119b
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-20 09:45:17 -0300

    fix small

--- src/data/metadata.py
+++ src/data/metadata.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import os
 
-crsp_stocks = list(pd.read_csv(os.path.join(os.path.dirname(__file__), "inputs", "crsp_example.csv"), usecols=["ticker"])["ticker"].unique())
+crsp_stocks = list(pd.read_csv(os.path.join(os.path.dirname(__file__), "inputs", "crsp_example.csv"), usecols=["ticker"])["ticker"].unique())[0:100]
