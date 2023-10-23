e2dde912186aea2b413366a7f520ddaf7ad20584
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-23 16:16:15 -0300

    change default bootstrap method

--- src/run_rbmvo.py
+++ src/run_rbmvo.py
@@ -16,7 +16,7 @@ parser.add_argument('-mn', '--model_name', type=str, help='model name to be used
 parser.add_argument('-nti', '--num_timesteps_in', type=int, help='size of the lookback window for the time series data', default=252 * 1)
 parser.add_argument('-nto', '--num_timesteps_out', type=int, help='size of the lookforward window to be predicted', default=1)
 parser.add_argument('-lo', '--long_only', type=str, help='consider long only constraint on the optimization', default="True")
-parser.add_argument('-meancove', '--mean_cov_estimator', type=str, help='name of the estimator to be used for the expected returns', default="nobb")
+parser.add_argument('-meancove', '--mean_cov_estimator', type=str, help='name of the estimator to be used for the expected returns', default="cbb")
 
 if __name__ == "__main__":
 
