def3d59445f8d6612adbe4754370676f2f3e82ba
grover002 <grover.org.rf@gmail.com>
2023-10-24 22:08:43 +0200

    new RBMVO

---
+++ src/models/nRBMVO.py
@@ -0,0 +1,117 @@
+import torch
+import numpy as np
+import scipy.optimize as opt
+
+from estimators.Estimators import Estimators
+from functionals.Functionals import Functionals
+
+class nRBMVO(Estimators, Functionals):
+    def __init__(self,
+                 risk_aversion: float=1,
+                 mean_cov_estimator: str="mle",
+                 num_boot: int = 200,
+                 alpha: float = 0.05,
+                 mean_functional: str=None,
+                 cov_functional: str=None) -> None:
+        """"
+        This function impements the robust version of the mean-variance optimization (MVO) method proposed by Markowitz (1952).
+        It intends to be robust in two senses:
+            1. It uses a bootstrap procedure to estimate confidence intervals of the mean and covariance of returns.
+            2. It uses the alpha-percentile mean and covariance as a plug-in estimator for the mean and covariance of returns.
+
+        Optimization can be done using: (a) gradient descent or (b) using the scipy.optimize.minimize function.
+
+        Args:
+            risk_aversion (float): risk aversion parameter. Defaults to 0.5. 
+                                   The risk aversion parameter is a scalar that controls the trade-off between risk and return.
+                                   According to Ang (2014), the risk aversion parameter of a risk neutral individual ranges from 1 and 10.
+            mean_estimator (str): mean estimator to be used. Defaults to "mle", which defines the maximum likelihood estimator.
+            covariance_estimator (str): covariance estimator to be used. Defaults to "mle", which defines the maximum likelihood estimator.
+            num_boot (int): number of bootstrap samples to be used. Defaults to 200.
+            alpha (float): percentile. Defaults to 0.95.
+            mean_functional (str): functional to be used to rank the mean of the returns. Defaults to "means".
+            cov_functional (str): functional to be used to rank the covariance of the returns. Defaults to "eigenvalues".
+            
+        References:
+        Markowitz, H. (1952) Portfolio Selection. The Journal of Finance.
+        Ang, Andrew, (2014). Asset Management: A Systematic Approach to Factor Investing. Oxford University Press. 
+        """
+
+        Estimators.__init__(self)
+
+        Functionals.__init__(self, alpha=alpha)
+        
+        self.risk_aversion = risk_aversion
+        self.mean_cov_estimator = mean_cov_estimator 
+        self.covariance_estimator = mean_cov_estimator
+        self.num_boot = num_boot
+        self.estimated_means = list()
+        self.estimated_covs = list()
+        self.mean_functional = mean_functional
+        self.cov_functional = cov_functional
+
+        if self.mean_functional == self.cov_functional:
+            self.functional = self.mean_functional
+
+    def forward(self,
+                returns: torch.Tensor,
+                num_timesteps_out: int,
+                long_only: bool=True):
+             
+        self.K = returns.shape[1]
+
+        # mean and cov estimates
+        if self.mean_cov_estimator == "mle":
+            self.list_mean_covs = [(self.MLEMean(returns),self.MLECovariance(returns))]
+        else:
+            self.list_mean_covs = self.DependentBootstrapMean_Covariance(returns=returns,
+                                                                         boot_method=self.mean_cov_estimator,
+                                                                         Bsize=50,
+                                                                         rep=self.num_boot)
+        
+        # compute the means and eigenvalues, and select the alpha-percentile of them
+        def true_objective(weights: torch.Tensor,
+                           maximize: bool=True) -> torch.Tensor:
+            c = -1 if maximize else 1
+            # define the utility function internally
+            def utility_fn(w:torch.Tensor,
+                           mean_t:torch.Tensor,
+                           cov_t:torch.Tensor) -> torch.Tensor:
+                return (np.dot(mean_t, w) - ((self.risk_aversion/2) * np.dot(w, np.dot(cov_t, w)))) * c
+            # compute the utility for all
+            utilities = list()
+            for idx in range(len(self.list_mean_covs)):
+                mean_t,cov_t = self.list_mean_covs[idx]
+                utility = utility_fn(weights,mean_t,cov_t)
+                utilities.append(utility)
+            # sort utilities
+            utilities.sort()
+            #print(len(utilities))
+            # return the utility of the alpha IC
+            if len(utilities) == 1:
+                return utilities[0]
+            else:
+                return utilities[int((self.alpha)*len(utilities))]
+            
+        #
+        if long_only:
+            constraints = [
+                {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}  # the weights sum to one
+            ]
+            bounds = [(0, None) for _ in range(self.K)]
+
+            w0 = np.random.uniform(0, 1, size=self.K)
+        else:
+            constraints = [
+                {'type': 'eq', 'fun': lambda x: np.sum(x) - 0},  # the weights sum to zero
+                {'type': 'eq', 'fun': lambda x: np.sum(np.abs(x)) - 1},  # the weights sum to zero
+            ]
+            bounds = [(-1, 1) for _ in range(self.K)]
+
+            w0 = np.random.uniform(-1, 1, size=self.K)
+
+        # perform the optimization
+        opt_output = opt.minimize(true_objective, w0, constraints=constraints, bounds=bounds, method='SLSQP')
+        wt = torch.tensor(np.array(opt_output.x)).T.repeat(num_timesteps_out, 1)
+
+        return wt
---
+++ src/run_nrbmvo.py
@@ -0,0 +1,129 @@
+import os
+import pandas as pd
+import torch
+import argparse
+from tqdm import tqdm
+
+from models.nRBMVO import nRBMVO
+from data.ETFsLoader import ETFsLoader
+from utils.dataset_utils import create_rolling_window_ts, check_bool
+from loss_functions.SharpeLoss import SharpeLoss
+from utils.conn_data import save_result_in_blocks
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-mn', '--model_name', type=str, help='model name to be used for saving the model', default="nrbmvo")
+parser.add_argument('-nti', '--num_timesteps_in', type=int, help='size of the lookback window for the time series data', default=252 * 1)
+parser.add_argument('-nto', '--num_timesteps_out', type=int, help='size of the lookforward window to be predicted', default=1)
+parser.add_argument('-lo', '--long_only', type=str, help='consider long only constraint on the optimization', default="True")
+parser.add_argument('-meancove', '--mean_cov_estimator', type=str, help='name of the estimator to be used for the expected returns', default="cbb")
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+
+    model_name = args.model_name
+    train_ratio = 0.6
+    num_timesteps_in = args.num_timesteps_in
+    num_timesteps_out = args.num_timesteps_out
+    fix_start = False
+    drop_last = True
+    long_only = check_bool(args.long_only)
+    mean_cov_estimator = args.mean_cov_estimator
+    alpha = 0.95
+    mean_functional = "means"
+    cov_functional = "eigenvalues"
+
+    print("Running script with the following parameters: model_name: {}, long_only: {}, mean_cov_estimator: {}".format(model_name, long_only, mean_cov_estimator))
+
+    # add tag for long only or long-short portfolios
+    model_name = "{model_name}_lo".format(model_name=model_name) if long_only else "{model_name}_ls".format(model_name=model_name)
+
+    # add mean estimator tag to name
+    model_name = "{model_name}_{mean_cov_estimator}".format(model_name=model_name, mean_cov_estimator=mean_cov_estimator)
+    
+    args.model_name = model_name
+
+    # relevant paths
+    source_path = os.path.dirname(__file__)
+    inputs_path = os.path.join(source_path, "data", "inputs")
+
+    # prepare dataset
+    loader = ETFsLoader()
+    returns = loader.returns.T
+    features = loader.features
+    features = features.reshape(features.shape[0], features.shape[1] * features.shape[2]).T  
+
+    X_steps, returns_steps = create_rolling_window_ts(features=features, 
+                                                      target=returns,
+                                                      num_timesteps_in=num_timesteps_in,
+                                                      num_timesteps_out=num_timesteps_out,
+                                                      fix_start=fix_start,
+                                                      drop_last=drop_last)
+
+    # (1) call model
+    model = nRBMVO(mean_cov_estimator=mean_cov_estimator, alpha=alpha, mean_functional=mean_functional, cov_functional=cov_functional)
+
+    # (2) loss fucntion
+    lossfn = SharpeLoss()
+
+    # (3) training/validation + oos testing
+    test_weights = torch.zeros((X_steps.shape[0], num_timesteps_out, X_steps.shape[2]))
+    test_returns = torch.zeros((X_steps.shape[0], num_timesteps_out, X_steps.shape[2]))
+    test_loss = torch.zeros((X_steps.shape[0], num_timesteps_out))
+    pbar = tqdm(range(X_steps.shape[0]), total=(X_steps.shape[0] + 1))
+    for step in pbar:
+        X_t = X_steps[step, :, :]
+        returns_t1 = returns_steps[step, :, :]
+
+        weights_t1 = model.forward(returns=X_t, num_timesteps_out=num_timesteps_out, long_only=long_only)
+        test_weights[step, :, :] = weights_t1
+
+        loss = lossfn(weights=weights_t1, returns=returns_t1)
+        test_returns[step, :, :] = returns_t1
+
+        test_loss[step, :] = loss.item()
+
+        pbar.set_description("Steps: %d, Test sharpe : %1.5f" % (step, loss.item()))
+
+    if test_weights.dim() == 3:
+        weights = test_weights.reshape(test_weights.shape[0] * test_weights.shape[1], test_weights.shape[2])
+    else:
+        weights = test_weights
+
+    if test_returns.dim() == 3:
+        returns = test_returns.reshape(test_weights.shape[0] * test_weights.shape[1], test_weights.shape[2])
+    else:
+        returns = test_returns
+
+    # (4) save results
+    returns_df = pd.DataFrame(returns.numpy(), index=loader.index[(num_timesteps_in + num_timesteps_out):], columns=loader.columns)
+    weights_df = pd.DataFrame(weights.numpy(), index=loader.index[(num_timesteps_in + num_timesteps_out):], columns=loader.columns)
+    
+    melt_returns_df = returns_df.reset_index().melt("index").rename(columns={"index": "date", "variable": "ticker", "value": "returns"})
+    melt_weights_df = weights_df.reset_index().melt("index").rename(columns={"index": "date", "variable": "ticker", "value": "weights"})
+    summary_df = pd.merge(melt_returns_df, melt_weights_df, on=["date", "ticker"], how="inner")
+
+    results = {
+        
+        "model": model,
+        "means": None,
+        "covs": None,
+        "train_loss": None,
+        "eval_loss": None,
+        "test_loss": test_loss,
+        "returns": returns_df,
+        "weights": weights_df,
+        "summary": summary_df
+
+        }
+
+    output_path = os.path.join(os.path.dirname(__file__),
+                               "data",
+                               "outputs",
+                               model_name)
+    
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    
+    save_result_in_blocks(results=results, args=args, path=output_path)
