22e4a0d79ad2a8ff1d69f4dbeac1707334d04340
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-29 09:42:05 -0300

    add make scripts

---
+++ src/run_experiment_mvo.sh
@@ -0,0 +1,8 @@
+python run_mvo.py --model_name mvo --long_only True --mean_estimator mle --cov_estimator mle
+python run_mvo.py --model_name mvo --long_only False --mean_estimator mle --cov_estimator mle
+python run_mvo.py --model_name mvo --long_only True --mean_estimator cbb --cov_estimator cbb
+python run_mvo.py --model_name mvo --long_only False --mean_estimator cbb --cov_estimator cbb
+python run_mvo.py --model_name mvo --long_only True --mean_estimator nobb --cov_estimator nobb
+python run_mvo.py --model_name mvo --long_only False --mean_estimator nobb --cov_estimator nobb
+python run_mvo.py --model_name mvo --long_only True --mean_estimator sb --cov_estimator sb
+python run_mvo.py --model_name mvo --long_only False --mean_estimator sb --cov_estimator sb
---
+++ src/run_experiment_rbmvo.sh
@@ -0,0 +1,16 @@
+python run_rbmvo.py --model_name rbmvo --long_only True --mean_cov_estimator mle --alpha 0.95
+python run_rbmvo.py --model_name rbmvo --long_only True --mean_cov_estimator mle --alpha 0.75
+python run_rbmvo.py --model_name rbmvo --long_only False --mean_cov_estimator mle --alpha 0.95
+python run_rbmvo.py --model_name rbmvo --long_only False --mean_cov_estimator mle --alpha 0.75
+python run_rbmvo.py --model_name rbmvo --long_only True --mean_cov_estimator cbb --alpha 0.95
+python run_rbmvo.py --model_name rbmvo --long_only True --mean_cov_estimator cbb --alpha 0.75
+python run_rbmvo.py --model_name rbmvo --long_only False --mean_cov_estimator cbb --alpha 0.95
+python run_rbmvo.py --model_name rbmvo --long_only False --mean_cov_estimator cbb --alpha 0.75
+python run_rbmvo.py --model_name rbmvo --long_only True --mean_cov_estimator nobb --alpha 0.95
+python run_rbmvo.py --model_name rbmvo --long_only True --mean_cov_estimator nobb --alpha 0.75
+python run_rbmvo.py --model_name rbmvo --long_only False --mean_cov_estimator nobb --alpha 0.95
+python run_rbmvo.py --model_name rbmvo --long_only False --mean_cov_estimator nobb --alpha 0.75
+python run_rbmvo.py --model_name rbmvo --long_only True --mean_cov_estimator sb --alpha 0.95
+python run_rbmvo.py --model_name rbmvo --long_only True --mean_cov_estimator sb --alpha 0.75
+python run_rbmvo.py --model_name rbmvo --long_only False --mean_cov_estimator sb --alpha 0.95
+python run_rbmvo.py --model_name rbmvo --long_only False --mean_cov_estimator sb --alpha 0.75
