8549c8aab7d62741e1b0ff1e6a7411e1b89b5a50
dcuoliveira <dan.cunhaa@gmail.com>
2024-02-24 09:00:55 -0300

    add sh and fix model name

---
+++ src/run_experiment_cmvo.sh
@@ -0,0 +1,9 @@
+python run_CucuringuclusterMVO.py --model_name cmvo --long_only True --mean_cov_estimator mle
+python run_CucuringuclusterMVO.py --model_name cmvo --long_only False --mean_cov_estimator mle
+python run_CucuringuclusterMVO.py --model_name cmvo --long_only True --mean_cov_estimator cbb
+python run_CucuringuclusterMVO.py --model_name cmvo --long_only False --mean_cov_estimator cbb
+python run_CucuringuclusterMVO.py --model_name cmvo --long_only True --mean_cov_estimator nobb
+python run_CucuringuclusterMVO.py --model_name cmvo --long_only False --mean_cov_estimator nobb
+python run_CucuringuclusterMVO.py --model_name cmvo --long_only True --mean_cov_estimator sb
+python run_CucuringuclusterMVO.py --model_name cmvo --long_only False --mean_cov_estimator sb
+
