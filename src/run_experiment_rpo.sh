54555cc82ba8506268fea6fb2d07ce64abdeed86
dcuoliveira <dan.cunhaa@gmail.com>
2024-12-02 20:38:40 +0000

    fix runenrs

--- src/run_experiment_rpo.sh
+++ src/run_experiment_rpo.sh
@@ -1,8 +1,2 @@
 python run_rpo.py --model_name rpo --long_only True --mean_estimator mle --cov_estimator mle
 python run_rpo.py --model_name rpo --long_only False --mean_estimator mle --cov_estimator mle
-python run_rpo.py --model_name rpo --long_only True --mean_estimator cbb --cov_estimator cbb
-python run_rpo.py --model_name rpo --long_only False --mean_estimator cbb --cov_estimator cbb
-python run_rpo.py --model_name rpo --long_only True --mean_estimator nobb --cov_estimator nobb
-python run_rpo.py --model_name rpo --long_only False --mean_estimator nobb --cov_estimator nobb
-python run_rpo.py --model_name rpo --long_only True --mean_estimator sb --cov_estimator sb
-python run_rpo.py --model_name rpo --long_only False --mean_estimator sb --cov_estimator sb
